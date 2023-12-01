7281a90e9f043ae718f1f5f27968a1bcc70893d0
Mathias <mthsvilly@gmail.com>
2023-12-01 14:27:03 +0100

    Mise en place de la Presentation

---
+++ portfolio/src/components/Presentation/index.jsx
@@ -0,0 +1,21 @@
+import "./presentation.scss";
+function Presentation() {
+  return (
+    <section className="presentation_align" id="presentation">
+      <h2 className="presentation_title">PRESENTATION</h2>
+      <div className="presentation_line"></div>
+      <br />
+      <article className="presentation_description">
+        Bonjour,
+        <br /> Je suis un passionné d'informatique et je me suis lancé dans une
+        formation <strong>d'intégrateur web</strong> il y a neuf mois.
+        <br /> Mon parcours est axé sur l'apprentissage des compétences
+        nécessaires pour devenir un intégrateur web <strong>compétent </strong>
+        et
+        <strong> polyvalent</strong>.
+      </article>
+    </section>
+  );
+}
+
+export default Presentation;
